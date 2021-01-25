558dda4364cfc8f218c5871a2377772bcbaca4be
Mika <>
2021-01-25 16:04:24 +0100

    fetch logs and display them on screen

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,27 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
 
 const Logs = () => {
     const [logs, setLogs] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        getLogs();
+        // eslint-disable-next-line
+    }, []);
+
     const getLogs = async () => {
         setLoading(true);
         const res = await fetch('/logs');
-        const data = res.json();
+        const data = await res.json();
 
         setLogs(data);
         setLoading(false);
     };
 
-    if(loading) {
-        <h4>Loading...<</h4>
+    if (loading) {
+        return <Preloader />
     }
 
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <ul className="collection with-header">
+            <li className="collection-header">
+                <h4 className="center">System Logs</h4>
+            </li>
+            {!loading && logs.length === 0 ? 
+            (<p className="center">No logs to show...</p>)
+            :
+            (logs.map(log => <LogItem log={log} key={log.id} />))
+            }
+        </ul>
+    );
+};
 
-export default Logs
+export default Logs;
